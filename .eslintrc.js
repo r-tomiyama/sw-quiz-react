4700a64d9b2b2a403c6fa073b144a0bec2b276e9
r-tomiyama <r.tomiyama@opt.ne.jp>
2020-11-05 16:37:55 +0900

    setup linter

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  env: {
+    es6: true,
+    node: true,
+    jest: true
+  },
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-use-before-define': 'off'
+  }
+}
